538a57c2cb30ad0e0db994617ed8322d5a3b9c94
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-03-30 16:55:27 +0200

    sadasd

--- codewars.js
+++ codewars.js
@@ -1,9 +1,22 @@
 
 
-function stringToArray(string){
+function countPositivesSumNegatives(input) {
+  // your code here
+  let tempArray = [];
+  let countPositives = 0;
 
-	// code code code
-  let tempArray=[]
-  let splitString = string.split(' ');
-  tempArray.push(splitString)
+    if (element > 0) {
+      for (let i = 0; i < input.length; i++) {
+        const element = input[i];
+        
+      }
+
+      let result= element += countPositives;
+      tempArray.push(result)
+    }
+  
+  return tempArray
 }
+
+console.log(countPositivesSumNegatives[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+
