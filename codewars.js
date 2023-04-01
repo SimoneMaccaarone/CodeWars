7deb3f08d8bc4fdb7ab2a329a0ed8820ea98c6d8
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-01 10:31:50 +0200

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,14 +1,41 @@
-const areaOrPerimeter = function(l , w) {
-  // Return your answer
-//   let squareArea = 0;
-     let rettangoloPerimetro = 0
-  if( l !== w){
-    rettangoloPerimetro = (l*2) + (w*2)
-    return rettangoloPerimetro;
-  }
-  const squareArea = l*w;
-  return squareArea;
-        
-};
 
-console.log(areaOrPerimeter(3,4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------
+// function basicOp(operation, value1, value2){
+//   if (operation === '+') {
+//     return value1 + value2
+//   } else if(operation==='-'){
+//     return value1 - value2
+//   }else if (operation==='*'){
+//     return value1 * value2
+//   }else{
+//     return value1 / value2
+//   }
+// }
+//------------------------------------------------------------
+// // AREA QUADRATO e RETTANGOLO 
+// const areaOrPerimeter = function(l , w) {
+//   // Return your answer
+// //   let squareArea = 0;
+//      let rettangoloPerimetro = 0
+//   if( l !== w){
+//     rettangoloPerimetro = (l*2) + (w*2)
+//     return rettangoloPerimetro;
+//   }
+//   const squareArea = l*w;
+//   return squareArea;
+// };
+// console.log(areaOrPerimeter(3,3))
