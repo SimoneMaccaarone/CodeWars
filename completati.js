eb7b2601f5542cbf87f8ac4f5d466bbb91be22e3
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-04 08:57:52 +0200

    carta-forbici-sasso

---
+++ completati.js
@@ -0,0 +1,101 @@
+
+// //      CARTA FORBICI SASSO
+// const rps = (p1, p2) => {
+//     //   let Array=['paper','scissors','rock']
+//       if(p1 === 'paper' && p2=== 'rock'){
+//         return 'Player 1 won!'
+//       }
+//       if(p1==='paper'&& p2==='scissors'){
+//         return 'Player 2 won!'
+//       }
+//       if(p1===p2){
+//         return 'Draw!'
+//       }
+
+//       if(p1 === 'scissors' && p2=== 'paper'){
+//         return 'Player 1 won!'
+//       }
+//       if(p1==='scissors'&& p2==='rock'){
+//         return 'Player 2 won!'
+//       }
+//       if(p1===p2){
+//         return 'Draw!'
+//       }
+//       if(p1 === 'rock' && p2=== 'scissors'){
+//         return 'Player 1 won!'
+//       }
+//       if(p1==='rock'&& p2==='paper'){
+//         return 'Player 2 won!'
+//       }
+//       if(p1===p2){
+//         return 'Draw!'
+//       }
+      
+//     };
+
+// console.log(rps('paper','rock'))
+// console.log(rps('paper','scissors'))
+// console.log(rps('paper','paper'))
+
+// console.log(rps('rock','rock'))
+
+
+// //      FUNZIONE NUMERI RANDOMICI
+// function getAge(inputString){
+
+// let getAge = Math.floor(Math.random() * 10)+1;
+// if (getAge===1) {
+//     return `${getAge} year old`
+// }
+// return `${getAge} years old`
+// }
+
+// console.log(getAge());
+
+// console.log(Math.floor(Math.random() * 9)+1);
+//------------------------------------------------------------
+// function basicOp(operation, value1, value2){
+//   if (operation === '+') {
+//     return value1 + value2
+//   } else if(operation==='-'){
+//     return value1 - value2
+//   }else if (operation==='*'){
+//     return value1 * value2
+//   }else{
+//     return value1 / value2
+//   }
+// }
+//------------------------------------------------------------
+// // AREA QUADRATO e RETTANGOLO 
+//------------- area e perimetro -------------
+// var countSheep = function (num){
+//   //your code here
+//   let text = "";
+//   for (let i = 0; i < num; i++) {
+//   text += i + 'sheep...';
+
+// }
+// return text
+
+// }
+
+// console.log(countSheep(5));
+
+
+//------------- area e perimetro -------------
+// const areaOrPerimeter = function(l , w) {
+//   // Return your answer
+// //   let squareArea = 0;
+//      let rettangoloPerimetro = 0
+//   if( l !== w){
+//     rettangoloPerimetro = (l*2) + (w*2)
+//     return rettangoloPerimetro;
+//   }
+//   const squareArea = l*w;
+//   return squareArea;
+// };
+// console.log(areaOrPerimeter(3,3))
+        
+// };
+
+// console.log(areaOrPerimeter(3,4))
