4f184ee9640e160c7ad49b3aced0b0bcfab8fe41
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-09 21:41:54 +0200

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,4 +1,21 @@
-var number=function(array){
-    for (let i=0; i< array.length; i++)
-       
-  }
+
+
+
+
+
+//---- rimuovi il piu piccolo senza modificare l' originale----
+// let numbers = [3, 2, 1, 4, 5,1 ]
+
+// function removeSmallest(numbers) {
+//   let tempArray = [...numbers]
+//   for (var i = 0; i < tempArray.length; i++);
+//   const element = tempArray[i];
+  
+//   let minNumber = Math.min(...tempArray); 
+
+//   let numerToRemove = tempArray.indexOf(minNumber);
+//   tempArray.splice(numerToRemove,1)
+//   return tempArray
+// }
+
+// console.log(removeSmallest(numbers));
