d8008b35bdd7ecdac9c58766c079286d5cb06a49
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-12 13:25:18 +0200

    ò

--- codewars.js
+++ codewars.js
@@ -3,19 +3,24 @@
 
 
 
-//---- rimuovi il piu piccolo senza modificare l' originale----
-// let numbers = [3, 2, 1, 4, 5,1 ]
 
-// function removeSmallest(numbers) {
-//   let tempArray = [...numbers]
-//   for (var i = 0; i < tempArray.length; i++);
-//   const element = tempArray[i];
-  
-//   let minNumber = Math.min(...tempArray); 
 
-//   let numerToRemove = tempArray.indexOf(minNumber);
-//   tempArray.splice(numerToRemove,1)
-//   return tempArray
-// }
 
-// console.log(removeSmallest(numbers));
+
+
+
+
+
+
+
+
+function getCount(str) {
+  for (let i = 0; i < str.length; i++) {
+    
+  }
+}
+
+
+console.log(getCount('Walterino'))
+
+
--- completati.js
+++ completati.js
@@ -1,4 +1,19 @@
+//---- rimuovi il piu piccolo senza modificare l' originale----
+// let numbers = [3, 2, 1, 4, 5,1 ]
 
+// function removeSmallest(numbers) {
+//   let tempArray = [...numbers]
+//   for (var i = 0; i < tempArray.length; i++);
+//   const element = tempArray[i];
+  
+//   let minNumber = Math.min(...tempArray); 
+
+//   let numerToRemove = tempArray.indexOf(minNumber);
+//   tempArray.splice(numerToRemove,1)
+//   return tempArray
+// }
+
+// console.log(removeSmallest(numbers));
 // //      CARTA FORBICI SASSO
 // const rps = (p1, p2) => {
 //     //   let Array=['paper','scissors','rock']
